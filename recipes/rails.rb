089df83a633b9df0a816960a30c976b022c7aaa4
Timur Vafin <timur.vafin@flatstack.com>
2013-03-26 03:12:37 +0400

    Add firewall Add sysctl config Add fail2ban

--- recipes/rails.rb
+++ recipes/rails.rb
@@ -3,3 +3,8 @@ include_recipe 'nginx'
 directory '/etc/nginx/sites-include' do
   mode 0755
 end
+
+node.default.firewall.rules.push(
+  {'http' => {}},
+  {'https' => {}}
+)
--- recipes/security.rb
+++ recipes/security.rb
@@ -1,1 +1,4 @@
 include_recipe 'sudo'
+include_recipe 'ufw'
+include_recipe 'sysctl'
+include_recipe 'fail2ban'
