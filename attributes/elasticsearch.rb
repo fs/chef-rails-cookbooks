60ac0a0e9c91f91792a131dcec5f02c56ae1aafb
Timur Batyrshin <timur.batyrshin@flatstack.com>
2013-04-08 17:04:08 +0400

    single quotes -> double quotes in elasticsearch attributes

--- attributes/elasticsearch.rb
+++ attributes/elasticsearch.rb
@@ -1,3 +1,3 @@
-default.elasticsearch.version = "0.20.6"
+default.elasticsearch.version = '0.20.6'
 default.elasticsearch.cache_path = "/var/chef/elasticsearch-#{node.elasticsearch.version}.deb"
 default.elasticsearch.url = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-#{node.elasticsearch.version}.deb"
